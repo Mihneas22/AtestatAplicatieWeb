eeee50257af6f6ddb8942654a72e2d64d86ee92c
Mihneas22 <mihnea.serban06@gmail.com>
2025-02-06 19:21:26 +0200

    procese controllere

---
+++ WebAPI/Controllers/FoodController.cs
@@ -0,0 +1,34 @@
+﻿using Application.DTOs.FoodDTO.AddFood;
+using Application.DTOs.FoodDTO.GetFoodByName;
+using Application.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly IFood foodRepo;
+
+        public FoodController(IFood foodRepo)
+        {
+            this.foodRepo = foodRepo;
+        }
+
+        [HttpPost("addFood")]
+        public async Task<ActionResult<AddFoodResponse>> AddFoodAsync(AddFoodDTO addFoodDTO)
+        {
+            var result = await foodRepo.AddFoodAsync(addFoodDTO);
+            return Ok(result!);
+        }
+
+        [HttpGet("getFoodByName")]
+        public async Task<ActionResult<GetFoodByNameResponse>> GetFoodByNameAsync(GetFoodByNameDTO getFoodByNameDTO)
+        {
+            var result = await foodRepo.GetFoodByNameAsync(getFoodByNameDTO);
+            return Ok(result!);
+        }
+    }
+}
---
+++ WebAPI/Controllers/MealController.cs
@@ -0,0 +1,34 @@
+﻿using Application.DTOs.MealDTO.AddMeal;
+using Application.DTOs.MealDTO.GetRandomMeal;
+using Application.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealController : ControllerBase
+    {
+        private readonly IMeal mealRepo;
+
+        public MealController(IMeal mealRepo)
+        {
+            this.mealRepo = mealRepo;
+        }
+
+        [HttpPost("addMeal")]
+        public async Task<ActionResult<AddMealResponse>> AddMealAsync(AddMealDTO addMealDTO)
+        {
+            var result = await mealRepo.AddMealAsync(addMealDTO);
+            return Ok(result);
+        }
+
+        [HttpGet("getRandomMeal")]
+        public async Task<ActionResult<GetRandomMealResponse>> GetRandomMealAsync()
+        {
+            var result = await mealRepo.GetRandomMealAsync();
+            return Ok(result);
+        }
+    }
+}
